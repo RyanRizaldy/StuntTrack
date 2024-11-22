572cd948944c91389e801ef1fede2be46ccc8424
hizz <hizbif123@gmail.com>
2024-11-22 17:50:18 +0700

    Memindahkan file ke folder capstoneProject/MD

---
+++ capstoneProject/MD/app/src/main/java/com/bangkit/stuntack/ui/tracking/TrackingFragment.kt
@@ -0,0 +1,42 @@
+package com.bangkit.stuntack.ui.tracking
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.bangkit.stuntack.databinding.FragmentTrackingBinding
+
+class TrackingFragment : Fragment() {
+
+    private var _binding: FragmentTrackingBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val trackingViewModel =
+            ViewModelProvider(this).get(TrackingViewModel::class.java)
+
+        _binding = FragmentTrackingBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textTracking
+        trackingViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
