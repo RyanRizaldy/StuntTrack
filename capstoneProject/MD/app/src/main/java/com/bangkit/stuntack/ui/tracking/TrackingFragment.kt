a394060f19370a399eaa95788207443702c51404
RyanRizaldy <126148023+RyanRizaldy@users.noreply.github.com>
2024-11-25 16:30:20 +0700

    UI
    penambahan UI(tracking fragment,main activity,history fragment)

--- capstoneProject/MD/app/src/main/java/com/bangkit/stuntack/ui/tracking/TrackingFragment.kt
+++ capstoneProject/MD/app/src/main/java/com/bangkit/stuntack/ui/tracking/TrackingFragment.kt
@@ -28,10 +28,10 @@ class TrackingFragment : Fragment() {
         _binding = FragmentTrackingBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textTracking
-        trackingViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+//        val textView: TextView = binding.textTracking
+//        trackingViewModel.text.observe(viewLifecycleOwner) {
+//            textView.text = it
+//        }
         return root
     }
 
